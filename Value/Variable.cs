93d43949112766678481ba8057d24c4d8a2429b1
Kurash1 <clasheroff7@gmail.com>
2024-04-18 00:46:38 +0300

    .

--- Value/Variable.cs
+++ Value/Variable.cs
@@ -2,4 +2,5 @@
 public interface IVariable
 {
     public virtual Walker Call(Walker i, ref Block result) => throw new NotImplementedException();
+    public virtual bool LogicalCall(ref Walker i) => throw new NotImplementedException();
 }
